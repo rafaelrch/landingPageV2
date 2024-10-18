f984b8a66848fb5a5782ce0ae82e1f49412635c3
Rafael Rocha <rafaelrocha250304@gmail.com>
2024-10-17 22:54:58 -0300

    update QUEM SOMOS 2

--- src/components/QuemSomos.jsx
+++ src/components/QuemSomos.jsx
@@ -1,16 +1,36 @@
 
 function QuemSomos() {
   return (
-    <div className="Relative mt-20 border-b border-neutral-800 min-h-[800px]">
-      <div className="text-center">
+    <section>
+
+    <div className="Relative mt-20 border-b border-neutral-800 min-h-[800px] ">
+      <div className="text-center ">
         <span className="bg-neutral-900 text-purple-500 rounded-full h-6 text-sm font-medium px-4 py-2 uppercase border border-neutral-800/80">
           Quem Somos
         </span>
-        <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20">
-          Quem somos nós?
-        </h2>
+
+        <div className="bg-red-500 flex flex-row justify-center space-x-72 mx-10">
+
+          <div className="flex flex-col items-start bg-neutral-300 ml-20 gap-5 pb-10 ">
+            <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20">
+              Quem somos nós?
+            </h2>
+
+            <p className="text-start text-1xl sm:text-sm text-neutral-400">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />
+              Nostrum odit, provident soluta maiores, dolore sapiente <br /> facere beatae voluptatem id, 
+              adipisci mollitia illo asperiores in!  <br /> Quas eius nemo molestiae recusandae vel?
+            </p>
+
+          </div>
+
+
+            <div className="bg-neutral-800 w-1/4 h-96 my-5 border border-neutral-700/80" style={{ borderRadius: '60px' }}>
+            </div>
+          </div>
       </div>
     </div>
+    </section>
   )
 }
 
